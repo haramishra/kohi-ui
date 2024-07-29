47240f55f5426e0f66a9d272daad5927ec68ea41
Ashu <ashutoshmund55@gmail.com>
2024-07-30 01:40:13 +0530

    sections added

---
+++ src/app/ui/logocloud-sections/page.tsx
@@ -0,0 +1,18 @@
+const components = [
+  {
+    path: "https://kohi-unifyui.pages.dev/resources/logo-cloud/1/",
+    name: "Example 1",
+  },
+  {
+    path: "https://kohi-unifyui.pages.dev/resources/logo-cloud/2/",
+    name: "Example 2",
+  },
+  {
+    path: "https://kohi-unifyui.pages.dev/resources/logo-cloud/3/",
+    name: "Example 3",
+  },
+  {
+    path: "https://kohi-unifyui.pages.dev/resources/logo-cloud/4/",
+    name: "Example 4",
+  },
+];
