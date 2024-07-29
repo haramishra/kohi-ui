1a942d2e92ba1568f28053903281947fdc5f5b5a
Ashu <ashutoshmund55@gmail.com>
2024-07-30 02:06:56 +0530

    Function Addeed

--- src/app/ui/content-sections/page.tsx
+++ src/app/ui/content-sections/page.tsx
@@ -1,18 +1,22 @@
-const components = [
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/content/about/",
-    name: "About section",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/content/about-2/",
-    name: "About section2",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/content/about-3/",
-    name: "About section3",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/content/image-on-right/",
-    name: "Image on right",
-  },
-];
+export default function Content() {
+  const components = [
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/content/about/",
+      name: "About section",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/content/about-2/",
+      name: "About section2",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/content/about-3/",
+      name: "About section3",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/content/image-on-right/",
+      name: "Image on right",
+    },
+  ];
+
+  return <div></div>;
+}
--- src/app/ui/hero-sections/page.tsx
+++ src/app/ui/hero-sections/page.tsx
@@ -1,62 +1,66 @@
-const components = [
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/agency/",
-    name: "Agency Website",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/architecture/",
-    name: "Architecture",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/creative-agency/",
-    name: "Creative Agency",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/digital-agency/",
-    name: "Digital Agency",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/grocerystore/",
-    name: "Grocery Store",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/healthcare/",
-    name: "Health Care",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/hero-section/",
-    name: "Hero Section",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/medcare/",
-    name: "Medcare",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/personnal-porfolio/",
-    name: "Personnal Portfolio",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/podcast/",
-    name: "Podcast",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/podcast-agency/",
-    name: "Podcast Agency",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/saas/",
-    name: "Saas",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/saas-product/",
-    name: "Saas Product",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/software-agency/",
-    name: "Software Agency",
-  },
-  {
-    path: "https://kohi-unifyui.pages.dev/resources/hero-sections/with-slider/",
-    name: "With Slider",
-  },
-];
+export default function Hero() {
+  const components = [
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/agency/",
+      name: "Agency Website",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/architecture/",
+      name: "Architecture",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/creative-agency/",
+      name: "Creative Agency",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/digital-agency/",
+      name: "Digital Agency",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/grocerystore/",
+      name: "Grocery Store",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/healthcare/",
+      name: "Health Care",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/hero-section/",
+      name: "Hero Section",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/medcare/",
+      name: "Medcare",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/personnal-porfolio/",
+      name: "Personnal Portfolio",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/podcast/",
+      name: "Podcast",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/podcast-agency/",
+      name: "Podcast Agency",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/saas/",
+      name: "Saas",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/saas-product/",
+      name: "Saas Product",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/software-agency/",
+      name: "Software Agency",
+    },
+    {
+      path: "https://kohi-unifyui.pages.dev/resources/hero-sections/with-slider/",
+      name: "With Slider",
+    },
+  ];
+
+  return <div></div>;
+}
